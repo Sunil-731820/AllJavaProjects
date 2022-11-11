71149194f3f899f1d16591b642ba28197ac43c48
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-11-12 01:29:23 +0530

    coded again

---
+++ Spring-Boot-WebSocketProjectForChat/src/main/java/com/java/boot/SpringBootWebSocketProjectForChatApplication.java
@@ -0,0 +1,13 @@
+package com.java.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketProjectForChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocketProjectForChatApplication.class, args);
+	}
+
+}
