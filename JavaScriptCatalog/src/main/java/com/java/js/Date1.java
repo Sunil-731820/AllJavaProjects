509edb6850f24056f5128638396155d3171d2398
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-11-15 15:15:41 +0530

    coded again

--- JavaScriptCatalog/src/main/java/com/java/js/Date1.java
+++ JavaScriptCatalog/src/main/java/com/java/js/Date1.java
@@ -5,8 +5,8 @@ import java.time.format.DateTimeFormatter;
 
 public class Date1 {
 	public static void main(String[] args) {
-		LocalDateTime date1= LocalDateTime.now();
-        
+		
+		LocalDateTime date1= LocalDateTime.now(); 
         DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd-MM-yyyy : HH:mm:ss ");
         String formattedDateAndTime = date1.format(format1);
         System.out.println("formatted date and time : " + formattedDateAndTime);
