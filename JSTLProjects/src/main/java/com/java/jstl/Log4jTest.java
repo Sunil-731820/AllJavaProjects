eae97d3e91b14cb958902f0c9f687b711af93073
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-11-22 15:09:46 +0530

    coded again

---
+++ JSTLProjects/src/main/java/com/java/jstl/Log4jTest.java
@@ -0,0 +1,17 @@
+package com.java.jstl;
+
+import org.apache.logging.log4j.core.config.Loggers;
+import org.glassfish.hk2.utilities.reflection.Logger;
+
+public class Log4jTest {
+	
+	private static Logger log = Logger.getLogger();
+	public static void main(String[] args) {
+		System.out.println("The Use Of the Log4j In eclipse for debuggiing ");
+		
+		log.debug("Log4J Debug Message Test");
+		System.out.println("After Calling the Log.Debug()");
+		log.debug("Log4j info Message Tesst");
+	}
+
+}
