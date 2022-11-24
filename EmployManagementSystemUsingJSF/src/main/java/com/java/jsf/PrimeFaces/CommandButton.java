4acc805c60fde0b9a4ed608174b963904efa1d16
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-11-24 11:59:49 +0530

    coded again

---
+++ EmployManagementSystemUsingJSF/src/main/java/com/java/jsf/PrimeFaces/CommandButton.java
@@ -0,0 +1,19 @@
+package com.java.jsf.PrimeFaces;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+public class CommandButton {
+	
+	public void commandButtonAction(ActionEvent actionEvent) {
+		addMessage("You have clicked that Button");
+	}
+
+	public void addMessage(String summary) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,summary,null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+}
