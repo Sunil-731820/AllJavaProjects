e6f9ea0eeeed1aa272e5565fe7f1c21d1d094f8b
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-12-11 15:52:30 +0530

    coded

---
+++ JSFAndPrimeFacesProjects/src/main/java/com/java/jsf/PrimeFaces/BooleanCheckbox.java
@@ -0,0 +1,27 @@
+package com.java.jsf.PrimeFaces;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class BooleanCheckbox {
+	
+	private boolean value;
+	
+	public boolean isValue() {
+		return value;
+	}
+
+	public void setValue(boolean value) {
+		this.value = value;
+	}
+	
+	public void addMessage() {
+		String summary = value ? "Checked" : "UnChecked";
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(summary));
+	}
+
+}
